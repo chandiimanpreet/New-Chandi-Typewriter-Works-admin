ef2e2d25b3b94adcc8301db46f7410f7b8882cd3
chandiimanpreet <manpreetchandi555@gmail.com>
2023-12-27 23:31:26 +0530

    Implemented the settings page(Admin)

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -3,7 +3,7 @@ import { auth } from '@clerk/nextjs';
 
 import prismadb from '@/lib/prismadb';
 
-export async function POST(req: Request,) {
+export async function POST(req: Request) {
     try {
         const { userId } = auth();
         const body = await req.json();
